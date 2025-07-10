977d0531d7fbe29f71e71fee001b3be9df85733d
Jorge Roldan <roldanjrgl@gmail.com>
2025-07-09 22:03:44 -0400

    feat(tools): completed llm_with_tools.invoke(messages)

--- experimental/langchain_tools/3_pass_tool_output_to_model.py
+++ experimental/langchain_tools/3_pass_tool_output_to_model.py
@@ -1,3 +1,52 @@
 """
 https://python.langchain.com/docs/how_to/tool_results_pass_to_model/
 """
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+from langchain.chat_models import init_chat_model
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+from langchain_core.tools import tool
+
+
+@tool
+def add(a: int, b: int) -> int:
+    """Adds a and b."""
+    return a + b
+
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b."""
+    return a * b
+
+
+tools = [add, multiply]
+
+llm_with_tools = llm.bind_tools(tools)
+
+from langchain_core.messages import HumanMessage
+
+query = "What is 3 * 12? Also, what is 11 + 49?"
+
+messages = [HumanMessage(query)]
+
+ai_msg = llm_with_tools.invoke(messages)
+
+print(ai_msg.tool_calls)
+
+messages.append(ai_msg)
+print(messages)
+
+for tool_call in ai_msg.tool_calls:
+    selected_tool = {"add": add, "multiply": multiply}[tool_call["name"].lower()]
+    tool_msg = selected_tool.invoke(tool_call)
+    messages.append(tool_msg)
+
+print(messages)
+
+output = llm_with_tools.invoke(messages)
+print(output)
