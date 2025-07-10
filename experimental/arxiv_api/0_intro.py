2808ea16dd07c7ccf68a689415ef6d0b25ca92f6
Jorge Roldan <roldanjrg@protonmail.com>
2025-07-10 10:03:23 -0400

    feat(1_arxiv_demo): installed dependencies

--- experimental/arxiv_api/0_intro.py
+++ experimental/arxiv_api/0_intro.py
@@ -1,8 +1,17 @@
-import urllib
-import urllib.request
+import requests
 
 url = (
     "http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1"
 )
-data = urllib.request.urlopen(url)
-print(data.read().decode("utf-8"))
+
+# You can customize headers here
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+    "Accept": "application/xml,application/xhtml+xml,text/html;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "Connection": "keep-alive",
+}
+
+response = requests.get(url, headers=headers)
+print(response.text)
